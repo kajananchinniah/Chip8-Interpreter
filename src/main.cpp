16859a8f73e91fda990db8faa5445cd2208df56d
kajananchinniah <kajananchinniah@gmail.com>
2019-12-28 15:42:55 -0500

    More or less works

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include "Chip8.hpp"
 #include "GUI.hpp"
 
-int main(void)
+int main(int argc, char **argv)
 {
-    int scale = 10;
-    int delay = 1;
-    char const* file_path = "Tetris.ch8";
+    if (argc != 2)
+    {
+        std::cout << "Error: Input format is " << argv[0] << " rom_path\n";
+        return 0;
+    }
 
-    GUI gui("Chip 8", DISP_LENGTH*10, DISP_WIDTH*10);
+    int scale = 10;
+    int delay = 4;
+    char const* file_path = argv[1];
+
+    GUI gui("Chip 8", DISP_LENGTH*scale, DISP_WIDTH*scale);
     Chip8 chip8;
     chip8.load(file_path);
     bool quit = false;
@@ -16,9 +22,11 @@ int main(void)
         quit = gui.getInput(chip8.keypad);
         chip8.cycle();
         if (chip8.draw_flag == true)
+        {
             gui.update(chip8.display, &chip8.draw_flag, scale);
-        
-        SDL_Delay(10);
+        }
+
+        SDL_Delay(delay);
     }
 
     return 0;
